2ddf0efb458968172b85a2bb2a5930af7dd56a83
robertrebnor <robert.rebnor@bi.no>
2020-05-26 00:41:17 +0200

    first hist

--- testfile.py
+++ testfile.py
@@ -38,6 +38,38 @@ OkCupid[OkCupid.sex == "m"]
 # List unique 
 list( OkCupid["sex"].unique() )
 
+OkCupid.head(5)
+
+### Histogram:
+import matplotlib.pyplot as plt
+
+#def histPlot():
+    
+# Def
+x = OkCupid.sex
+
+len( OkCupid[OkCupid.sex == "m"] ) # 35829
+
+len( OkCupid[OkCupid.sex == "f"] ) # 24117
+
+len( OkCupid.sex ) # 59946
+
+
+# Number of bins
+num_bins = len( x.unique() )
+
+n, bins, rectangles = ax.hist(x, num_bins, density=True)
+
+plt.hist(x, density=True, bins=num_bins)
+
+
+plt.hist(x, bins=num_bins, )
+plt.ylabel('Frequency')
+plt.xlabel('Data')
+plt.title("Histogram")
+plt.show()
+
+
 
 
 
